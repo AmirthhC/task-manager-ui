62efebf2d6ee42affde235873987795858c430da
AmirthhC <amirthh.c@gmail.com>
2025-06-20 17:45:28 +0530

    Update FormElements.tsx

--- src/components/FormElements.tsx
+++ src/components/FormElements.tsx
@@ -25,6 +25,7 @@ export const Input = React.forwardRef<HTMLInputElement, InputProps>(
         </label>
         <input
           ref={ref}
+          disabled={loading}
           className={`w-full border rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 ${
             error ? "border-red-500" : "border-gray-300"
           } ${className}`}
